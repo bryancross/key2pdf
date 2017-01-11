1ea3ed3e7c228b34570339db5c77b8eb38f5878f
Bryan Cross <bryancross@github.com>
2017-01-11 08:12:39 -0600

    Added test scripts

--- script/bootstrap.sh
+++ script/bootstrap.sh
@@ -5,4 +5,5 @@ mkdir -p log
 echo "Creating jobs directory"
 mkdir -p jobs
 cp config/job-template-example.json config/job-temlplate.json
-echo "Remember to edit config/job-template.json with correct default values"
+echo "Remember to edit config/job-template.json and, optionally,"
+echo "test/test-params.json with correct default values"
