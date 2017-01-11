c94d20f8086087283dc9eb10f22226fc95c504d4
Bryan Cross <bryancross@github.com>
2017-01-11 11:44:56 -0600

    Fixing typo

--- script/bootstrap.sh
+++ script/bootstrap.sh
@@ -4,6 +4,6 @@ echo "creating log directory"
 mkdir -p log
 echo "Creating jobs directory"
 mkdir -p jobs
-cp config/job-template-example.json config/job-temlplate.json
+cp config/job-template-example.json config/job-template.json
 echo "Remember to edit config/job-template.json and, optionally,"
 echo "test/test-params.json with correct default values"
