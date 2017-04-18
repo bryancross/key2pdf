db2cfecac85be46b82624c5bb9a20749ccaf76ba
Bryan Cross <bryancross@github.com>
2017-04-17 20:51:08 -0500

    Updated test script

--- test/testKey2pdf.sh
+++ test/testKey2pdf.sh
@@ -1,1 +1,3 @@
-./script/key2pdf.sh convert-file https://github.com/bryancross/testrepo/blob/master/deck12.key --commit-after-convert --copy-to-gdrive
+./script/key2pdf.sh convert-file https://github.com/bryancross/testrepo/blob/master/foo/deck38.key --commit-after-convert --copy-to-gdrive
+./script/key2pdf.sh convert-file https://github.com/bryancross/testrepo/blob/master/foo/deck10.key --commit-after-convert --copy-to-gdrive
+./script/key2pdf.sh convert-file https://github.com/bryancross/testrepo/blob/master/foo/deck3.key --commit-after-convert --copy-to-gdrive
